c7a5100f19cd4c73bcc0ce56859fab1abe550f1d
Nop_one <68745798+xkp95175333@users.noreply.github.com>
2026-04-24 11:57:19 +0700

    Create GFnameWorld36Zoneptr.asm

---
+++ Delta_struct/logpng/GFnameWorld36Zoneptr.asm
@@ -0,0 +1,10 @@
+
+.std:0000000156A1D488 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00…                dq 200h dup(0)
+.std:0000000156A1E488 60 D2 B7 83 01 00 00 00      qword_156A1E488 dq 183B7D260h           ; DATA XREF: sub_140A82A80+DB9↑r
+.std:0000000156A1E488                                                                                         ; sub_140AE6790+61↑r ...
+.std:0000000156A1E490 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00…                dq 3FFh dup(0)
+.std:0000000156A20488 36 D2 B7 83 01 00 00 56                         qword_156A20488 dq 5600000183B7D236h    ; DATA XREF: sub_140A82A80:loc_140A8384C↑r
+.std:0000000156A20488                                                                                         ; sub_140AE6790:loc_140AE67FA↑r ...
+.std:0000000156A20490 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00…                dq 200h dup(0)
+.std:0000000156A21490 60 1C A8 40 01 00 00 00                                         dq offset ?Reserve@WriteBytesCount@AK@@UEAA_NJ@Z ; AK::WriteBytesCount::Reserve(long)
+.std:0000000156A21498                                                 ; v8_qjs::External qword_156A21498
